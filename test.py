d8f329de8bc82de202cf80782f223583ee7b7680
noahnisbet <noahnisbet3@gmail.com>
2020-11-18 21:46:20 -0500

    make it better

--- test.py
+++ test.py
@@ -4,8 +4,6 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, StandardScaler, OrdinalEncoder
-import h5py
-import pickle
 
 array2 = ('Apartment', 'Entire home/apt', 3, 1.0, 'Real Bed',
          'strict', True, 'NYC', 1, 
